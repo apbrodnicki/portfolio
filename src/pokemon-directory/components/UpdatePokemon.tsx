229e0d8ffbaf54ca037d4499d1c98674fadafbba
Alex Brodnicki <apbrodnicki@gmail.com>
2024-04-12 15:31:56 -0500

    Improve add autocomplete styling and performance.

--- src/pokemon-directory/components/UpdatePokemon.tsx
+++ src/pokemon-directory/components/UpdatePokemon.tsx
@@ -1,7 +1,8 @@
-import { Autocomplete, Box, Button, Grid, ListItem, Paper, TextField, Typography } from '@mui/material';
+import { Autocomplete, Box, Button, Grid, ListItem, Paper, TextField, Typography, createFilterOptions } from '@mui/material';
 import { useFetchAllPokemonNames } from 'pokemon-directory/api/useFetchAllPokemonNames';
 import { useFetchAllSprites } from 'pokemon-directory/api/useFetchAllSprites';
 import { PokemonListContext } from 'pokemon-directory/contexts/PokemonListContext';
+import { formatPokemonName } from 'pokemon-directory/helper/helper';
 import { useUpdatePokemonList } from 'pokemon-directory/hooks/useUpdatePokemonList';
 import type { PokemonAutocompleteItem } from 'pokemon-directory/models/models';
 import React, { useContext, useState } from 'react';
@@ -40,23 +41,38 @@ export const UpdatePokemon = (): React.JSX.Element => {
 		setAutocompleteKey(`key-${Math.random().toString(36).substring(2, 11)}`);
 	};
 
+	const filterOptions = createFilterOptions({
+		matchFrom: 'any',
+		ignoreCase: true,
+		limit: 151,
+	});
+
 	return (
 		<Grid container justifyContent='center'>
 			<Grid item xs={12} lg={5} mx={3}>
 				<Paper elevation={3} sx={{ m: 5, backgroundColor: '#B8D8D8' }}>
 					<Autocomplete
-						key={autocompleteKey}
-						options={autocompleteOptions}
 						multiple
 						disableCloseOnSelect
-						isOptionEqualToValue={(option, value) => option === value}
-						getOptionLabel={(option) => option.name}
-						onChange={(_, value) => { onAutocompleteChange(value); }}
+						filterOptions={filterOptions}
+						key={autocompleteKey}
+						options={autocompleteOptions}
+						isOptionEqualToValue={(option, value) => (option as PokemonAutocompleteItem).name === (value as PokemonAutocompleteItem).name}
+						getOptionLabel={(option) => (option as PokemonAutocompleteItem).name}
+						onChange={(_, value) => { onAutocompleteChange((value as PokemonAutocompleteItem[])); }}
 						renderOption={(props, option) => (
 							<ListItem {...props}>
-								<Typography mr={6}>#{option.pokedexNumber}</Typography>
-								<Typography flexGrow={1}>{option.name}</Typography>
-								<Box component='img' src={option.sprite} alt='sprite' mr={6} />
+								<Typography mr={3}>#{(option as PokemonAutocompleteItem).pokedexNumber}</Typography>
+								<Typography flexGrow={1}>{formatPokemonName((option as PokemonAutocompleteItem).name)}</Typography>
+								<Box
+									minWidth={100}
+									minHeight={100}
+									display='flex'
+									alignItems='center'
+									justifyContent='center'
+								>
+									<Box component='img' src={(option as PokemonAutocompleteItem).sprite} loading='lazy' alt='sprite' mr={1} />
+								</Box>
 							</ListItem>
 						)}
 						renderInput={(params) => (
@@ -70,7 +86,9 @@ export const UpdatePokemon = (): React.JSX.Element => {
 									onClick('add');
 									updateAutocompleteKey();
 								}}>
-									Submit
+									<Typography variant='body1'>
+										Submit
+									</Typography>
 								</Button>
 							</>
 						)}
@@ -80,10 +98,10 @@ export const UpdatePokemon = (): React.JSX.Element => {
 			<Grid item xs={12} lg={5} mx={3}>
 				<Paper elevation={3} sx={{ m: 5, backgroundColor: '#B8D8D8' }}>
 					<Autocomplete
-						key={autocompleteKey}
-						options={pokemonList}
 						multiple
 						disableCloseOnSelect
+						key={autocompleteKey}
+						options={pokemonList}
 						isOptionEqualToValue={(option, value) => option === value}
 						onChange={(_, value) => { onAutocompleteChange(value); }}
 						renderOption={(props, option) => (
@@ -102,7 +120,9 @@ export const UpdatePokemon = (): React.JSX.Element => {
 									onClick('remove');
 									updateAutocompleteKey();
 								}}>
-									Submit
+									<Typography variant='body1'>
+										Submit
+									</Typography>
 								</Button>
 							</>
 						)}
