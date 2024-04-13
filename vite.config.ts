7581c7b50bea9847ee8f3ebf9504fb4a424c6d40
Alex Brodnicki <apbrodnicki@gmail.com>
2024-04-13 02:17:56 -0500

    Try again.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
-import 'dotenv/config';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig({
-	plugins: [tsconfigPaths()],
-});
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), '');
 
+	return {
+		plugins: [tsconfigPaths()],
+		define: {
+			'import.meta.env.VITE_LEXICON_SERVER_PORT': JSON.stringify(env.VITE_LEXICON_SERVER_PORT),
+			'import.meta.env.VITE_EC2_INSTANCE_IP': JSON.stringify(env.VITE_EC2_INSTANCE_IP),
+		},
+	};
+});
