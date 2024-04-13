b98078d6a528e9024353cf69314404a324fdc530
Alex Brodnicki <apbrodnicki@gmail.com>
2024-04-13 02:05:06 -0500

    Try to fix undefined env vars in prod.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
+import replace from '@rollup/plugin-replace';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [tsconfigPaths()],
+	plugins: [
+		tsconfigPaths(),
+		replace({
+			preventAssignment: true,
+			values: {
+				'process.env.VITE_LEXICON_SERVER_PORT': JSON.stringify(process.env.VITE_LEXICON_SERVER_PORT),
+				'process.env.VITE_EC2_INSTANCE_IP': JSON.stringify(process.env.VITE_EC2_INSTANCE_IP)
+			}
+		})
+	],
 });
+
