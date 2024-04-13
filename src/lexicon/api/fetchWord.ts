4aa805b679583e44eed20c9b74d9ad54ee846dd7
Alex Brodnicki <apbrodnicki@gmail.com>
2024-04-13 01:42:49 -0500

    Update api call.

--- src/lexicon/api/fetchWord.ts
+++ src/lexicon/api/fetchWord.ts
@@ -1,6 +1,6 @@
 import type { GenericWordWrapping } from 'lexicon/models/genericModels';
 
 export const fetchWord = async (word: string): Promise<GenericWordWrapping> => {
-	const response = await fetch(`api/word?word=${encodeURIComponent(word)}`);
+	const response = await fetch(`//${import.meta.env.VITE_EC2_INSTANCE_IP}:${import.meta.env.VITE_LEXICON_SERVER_PORT}/api/word?word=${encodeURIComponent(word)}`);
 	return await response.json();
 };
