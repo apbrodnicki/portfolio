dd1a068a777693dc37e641de0b410ec5e46317fc
Alex Brodnicki <apbrodnicki@gmail.com>
2024-04-13 02:13:35 -0500

    Try another fix for prod.

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,8 @@
-import replace from '@rollup/plugin-replace';
+import 'dotenv/config';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
-		tsconfigPaths(),
-		replace({
-			preventAssignment: true,
-			values: {
-				'process.env.VITE_LEXICON_SERVER_PORT': JSON.stringify(process.env.VITE_LEXICON_SERVER_PORT),
-				'process.env.VITE_EC2_INSTANCE_IP': JSON.stringify(process.env.VITE_EC2_INSTANCE_IP)
-			}
-		})
-	],
+	plugins: [tsconfigPaths()],
 });
 
