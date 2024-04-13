806c9f6e5b12e4fab83feae9dc3bdd18dff5d0be
Alex Brodnicki <apbrodnicki@gmail.com>
2024-04-13 02:51:34 -0500

    testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig({
-	plugins: [tsconfigPaths()],
+export default defineConfig(({ command, mode }) => {
+	const env = loadEnv(mode, process.cwd(), '');
+
+	return {
+		plugins: [tsconfigPaths()],
+		define: {
+			__APP_ENV__: JSON.stringify(env.APP_ENV),
+		},
+	};
 });
