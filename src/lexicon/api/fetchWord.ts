09e51c447ee4838c012d8a8bcb86148ff9f4adfa
Alex Brodnicki <apbrodnicki@gmail.com>
2024-04-13 00:20:20 -0500

    Update scripts and fix api call.

--- src/lexicon/api/fetchWord.ts
+++ src/lexicon/api/fetchWord.ts
@@ -1,6 +1,6 @@
 import type { GenericWordWrapping } from 'lexicon/models/genericModels';
 
 export const fetchWord = async (word: string): Promise<GenericWordWrapping> => {
-	const response = await fetch(`http://localhost:5000/api/word?word=${encodeURIComponent(word)}`);
+	const response = await fetch(`api/word?word=${encodeURIComponent(word)}`);
 	return await response.json();
 };
