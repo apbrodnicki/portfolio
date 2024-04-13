2942cb6ec75ce7b8dd102884882273645ed88e30
Alex Brodnicki <apbrodnicki@gmail.com>
2024-04-13 02:40:50 -0500

    testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,6 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig(({ mode }) => {
-	const env = loadEnv(mode, process.cwd(), '');
-
-	return {
-		plugins: [tsconfigPaths()],
-		define: {
-			'import.meta.env.VITE_LEXICON_SERVER_PORT': JSON.stringify(env.VITE_LEXICON_SERVER_PORT),
-			'import.meta.env.VITE_EC2_INSTANCE_IP': JSON.stringify(env.VITE_EC2_INSTANCE_IP),
-		},
-	};
+export default defineConfig({
+	plugins: [tsconfigPaths()],
 });
