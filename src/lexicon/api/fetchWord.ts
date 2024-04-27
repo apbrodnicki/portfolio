3cfeb5a6b77f4307fef3f8c51de7635b441cb36d
Alex Brodnicki <apbrodnicki@gmail.com>
2024-04-26 21:06:35 -0500

    Remove base urls from .env.

--- src/lexicon/api/fetchWord.ts
+++ src/lexicon/api/fetchWord.ts
@@ -1,7 +1,7 @@
 import type { GenericWordWrapping } from 'lexicon/models/genericModels';
 
 export const fetchWord = async (word: string): Promise<GenericWordWrapping> => {
-	// .env not working in production right now, `${import.meta.env.DICTIONARY_BASE_URL}/${word}?key=${import.meta.env.DICTIONARY_TOKEN}`
+	// .env not working in production right now, import.meta.env.DICTIONARY_TOKEN
 	const response = await fetch(`https://www.dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=62ae6585-05f1-46c3-8cda-757eca0f6e87`);
 	return await response.json();
 };
