9eca41c74a67f79a587e688a9cd5f5cdd7c99845
Alex Brodnicki <apbrodnicki@gmail.com>
2024-04-26 20:57:05 -0500

    Restore vite config.

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,6 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig(({ mode }) => {
-	const env = loadEnv(mode, process.cwd(), '');
-
-	return {
-		plugins: [tsconfigPaths()],
-		define: {
-			VITE_LEXICON_SERVER_PORT: JSON.stringify(env.VITE_LEXICON_SERVER_PORT),
-			VITE_EC2_INSTANCE_IP: JSON.stringify(env.VITE_EC2_INSTANCE_IP),
-		},
-	};
+export default defineConfig({
+	plugins: [tsconfigPaths()],
 });
