eb3f5a5ea7ef3bc236d78ef8c32171768d0061b8
Alex Brodnicki <apbrodnicki@gmail.com>
2024-04-26 22:36:31 -0500

    Add back to top button to footer.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,26 @@
-import { Link, Paper, Typography } from '@mui/material';
+import KeyboardDoubleArrowUpIcon from '@mui/icons-material/KeyboardDoubleArrowUp';
+import { Box, Button, Link, Paper, Typography } from '@mui/material';
 import React from 'react';
 
 export const Footer = (): React.JSX.Element => {
+	const onClick = (): void => {
+		window.scroll({
+			top: 0,
+			behavior: 'smooth'
+		});
+	};
 	return (
 		<Paper elevation={3} sx={{ m: 2, p: 4, backgroundColor: '#888888' }}>
-			<Typography variant='subtitle2' align='center'>
+			<Box display='flex' alignItems='center' justifyContent='space-between'>
 				<Link href='mailto:apbrodnicki@gmail.com' color='#111111'>
-					Email me!
+					<Typography variant='subtitle2' align='center'>
+						Email me!
+					</Typography>
 				</Link>
-			</Typography>
+				<Button variant='contained' onClick={onClick}>
+					<KeyboardDoubleArrowUpIcon />
+				</Button>
+			</Box>
 		</Paper>
 	);
 };
