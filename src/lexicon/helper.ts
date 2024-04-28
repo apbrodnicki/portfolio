94f07290831080fb6c70b4320e3265a9fc3d97fd
Alex Brodnicki <apbrodnicki@gmail.com>
2024-04-28 03:58:19 -0500

    Remove console logs.

--- src/lexicon/helper.ts
+++ src/lexicon/helper.ts
@@ -3,8 +3,7 @@ import type { PartsOfSpeech, Word } from 'lexicon/models/models';
 
 export const filterWordsData = (word: GenericWordWrapping): Word => {
 	const innerWord = word[0];
-	console.log({ word });
-	console.log({ innerWord });
+
 	return {
 		id: innerWord.meta.id,
 		stems: innerWord.meta.stems,
