392720f7d7ac287f076c5d9e2c55f23fe050e313
Alex Brodnicki <apbrodnicki@gmail.com>
2024-09-22 16:44:11 -0500

    Remove unnecessary Home component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,33 @@
-import { Home } from 'components/Home';
+import { Box } from '@mui/material';
+import { About } from 'components/About';
+import { ErrorPage } from 'components/ErrorPage';
+import { Footer } from 'components/Footer';
+import { Header } from 'components/Header';
+import { Projects } from 'components/Projects';
 import React from 'react';
-import { useOutlet } from 'react-router-dom';
 import './App.css';
-// TODO:
-// Add custom theme.
+// TODO: Add custom theme.
 export const App = (): React.JSX.Element => {
-	const outlet = useOutlet();
+	const route = window.location.pathname;
+
+	if (route !== '/') {
+		return <ErrorPage error='Page not found.' />;
+	}
 
 	return (
-		<>
-			{outlet ?? <Home />}
-		</>
+		<Box id="portfolio">
+			<Header />
+			<Box
+				display='flex'
+				flexDirection='column'
+				justifyContent='center'
+				flex={1}
+				mt={3}
+			>
+				<About />
+				<Projects />
+			</Box>
+			<Footer />
+		</Box>
 	);
 };
