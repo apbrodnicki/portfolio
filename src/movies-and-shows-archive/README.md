ba51c4e5db22dbe9317321c43a3036190a16f5a3
Alex Brodnicki <apbrodnicki@gmail.com>
2024-09-22 16:43:30 -0500

    Abandon all hope with Movies and Shows Archive.

--- src/movies-and-shows-archive/README.md
+++
@@ -1,8 +0,0 @@
-# Movies and Shows Archive
-Placeholder description.
-
-The backend data is supplied by [The Movie Database](https://developer.themoviedb.org/).
-
-You may view the website [here](https://apbrodnicki.com/movies-and-shows-archive).
-___
-*Movies and Shows Archive was created by Alex Brodnicki.*
--- src/movies-and-shows-archive/api/fetchMovie.ts
+++
@@ -1,5 +0,0 @@
-export const fetchMovie = async (title: string, year: number): Promise<unknown> => {
-	// .env not working in production right now, import.meta.env.TMDB_TOKEN
-	const response = await fetch(`https://api.themoviedb.org/3/search/movie?query=${title}&api_key=395bbfebcfe3ef4bbf3a0b32ef848709&year=${year}`);
-	return await response.json();
-};
