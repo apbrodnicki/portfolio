4e1029e5e1ab44f088de83472736a120a0dac034
Alex Brodnicki <apbrodnicki@gmail.com>
2026-03-20 23:24:23 -0500

    Fix wording.

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -29,7 +29,7 @@ export const Projects = (): React.JSX.Element => (
 			/>
 			<DescriptionParagraph
 				text={
-					'I really enjoyed utilizing Material UI (a React component library) to a much fuller potential than I had previously. I fleshed out a Data Grid, built a highly customized Autocomplete input, and much more. Additionally, a big challenge I overcame with this project was learning how to call a custom hook in an event based manner, like through a button call. This means that even though the hook is called conditionally, it\'s still not breaking any of the rules of React.'
+					'I really enjoyed utilizing Material UI (a React component library) to a much fuller potential than I had previously. I fleshed out a Data Grid, built a highly customized Autocomplete input, and a lot more. Additionally, a big challenge I overcame with this project was learning how to call a custom hook in an event based manner, like through a button call. This means that even though the hook is called conditionally, it\'s still not breaking any of the rules of React.'
 				}
 			/>
 		</Box>
@@ -49,7 +49,7 @@ export const Projects = (): React.JSX.Element => (
 				m={2}
 			/>
 			<DescriptionParagraph
-				text='Lexicon is a word list app in which they can add words and have them saved long term, building their own personal dictionary. My primary motivation for creating Lexicon was to have a user interface that better suits my needs than any existing ones.'
+				text='Lexicon is a word list app in which users can add words and have them saved long term, building their own personal dictionary. My primary motivation for creating Lexicon was to have a user interface that better suits my needs than any existing ones.'
 			/>
 		</Box>
 		<Divider sx={{ borderColor: '#333333', borderWidth: '1px', width: '100%', margin: 'auto' }} />
